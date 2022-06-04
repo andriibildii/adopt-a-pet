cc7a8375c7b6fb92419c74cae9fe7d0a30e1fe3a
andriibildii <andrii.bildii@gmail.com>
2022-06-04 19:37:38 +0300

    using React Router (v5 - 5.2.0) to add client-side routing to a React Application

---
+++ src/pages/petDetailsNotFound/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+// import useHistory here.
+import { useHistory, } from "react-router-dom";
+
+const PetDetailsNotFound = () => {
+	// get the history object from useHistory()
+	const history = useHistory();
+
+	const goBack = () => {
+		history.goBack();
+	};
+
+	const goHome = () => {
+		history.push("/");
+	};
+
+	return (
+		<main className="page">
+			<h3>404: Who let the dogs out?</h3>
+			<p className="p404">
+				Sorry, but the details for this pet have not been uploaded by the
+				shelter yet. Check back later!
+			</p>
+			<img
+				className="img404"
+				src="https://i.chzbgr.com/full/8362031616/h9EB970C5/weve-lost-our-corgination"
+				alt=""
+			/>
+			<div className="actions-container">
+				<button className="button" onClick={goBack}>
+					Go Back
+				</button>
+				<button className="button" onClick={goHome}>
+					Go Home
+				</button>
+			</div>
+		</main>
+	);
+};
+
+export default PetDetailsNotFound;
